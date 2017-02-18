b29119f34e5ce73332dee5a8e31fbfb9e2dd6829
Mohammed Elsammak <mohammed.elsammak@modeso.ch>
2017-02-18 08:38:02 +0200

    Adding Entity layer.

--- Weather/Weather/Model/APIClient/APIClient.swift
+++ Weather/Weather/Model/APIClient/APIClient.swift
@@ -5,19 +5,25 @@
 //  Created by Mohammed Elsammak on 2/18/17.
 //  Copyright © 2017 elsammak. All rights reserved.
 //
-
+/// APICLient class is in charge of sending and receiving responses to/from Server
 import Foundation
 import Alamofire
 
 class APIClient {
-
+    /**
+     Request server for JSON response of weather information.
+     
+     - parameter location: Location that user made a query about.
+     
+     - returns: Completion blocks contains either data or error.
+     */
     public func getWeather(forLocation location: String, _ completion: @escaping Completion) {
         Alamofire.request(serverURL, method: .get, parameters: ["q": location], encoding: URLEncoding.default, headers: nil).validate().responseJSON { response in
 
             if let JSON = response.result.value {
-                completion(JSON as? NSDictionary, nil)
+                completion(JSON as? NSDictionary, nil) // Returns data for parsing
             } else {
-                completion(nil, response.error)
+                completion(nil, response.error) // Returns error for displaying
             }
         }
     }
--- Weather/Weather/Model/Entity/WeatherEntry.swift
+++ Weather/Weather/Model/Entity/WeatherEntry.swift
@@ -5,8 +5,14 @@
 //  Created by Mohammed Elsammak on 2/18/17.
 //  Copyright © 2017 elsammak. All rights reserved.
 //
-
+/// WeatherEntry object clas which contains attributes to display
 import Foundation
 public struct WeatherEntry {
 
+    var cityName: String?
+    var weatherIcon: String?
+    var humidity: String?
+    var weatherDescription: String?
+    var observationTime: String?
+    
 }
